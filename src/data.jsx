b6d4c18b56931afab28722ec4d74f7dcc0f6e7db
Donovan <donoriviere@gmail.com>
2023-07-26 10:26:02 +0200

    maj cv

--- src/data.jsx
+++ src/data.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { FaGithub, FaLinkedin, FaFilePdf } from 'react-icons/fa';
-const cv = <img src='https://img.icons8.com/glyph-neue/64/null/resume.png' />;
+import React from "react";
+import { FaGithub, FaLinkedin, FaFilePdf } from "react-icons/fa";
+const cv = <img src="https://img.icons8.com/glyph-neue/64/null/resume.png" />;
 
 export const links = [
   {
     id: 1,
-    url: '/',
-    text: 'présentation',
+    url: "/",
+    text: "présentation",
   },
   {
     id: 2,
-    url: '/projets',
-    text: 'projets',
+    url: "/projets",
+    text: "projets",
   },
   {
     id: 3,
-    url: '/formation',
-    text: 'formation',
+    url: "/formation",
+    text: "formation",
   },
   {
     id: 4,
-    url: '/experiences',
-    text: 'expériences',
+    url: "/experiences",
+    text: "expériences",
   },
 
   // {
@@ -34,17 +34,17 @@ export const links = [
 export const social = [
   {
     id: 1,
-    url: 'https://www.linkedin.com/in/donovan-rivi%C3%A8re-81664311b/',
+    url: "https://www.linkedin.com/in/donovan-rivi%C3%A8re-81664311b/",
     icon: <FaLinkedin />,
   },
   {
     id: 2,
-    url: 'https://github.com/Donovan-R',
+    url: "https://github.com/Donovan-R",
     icon: <FaGithub />,
   },
   {
     id: 3,
-    url: 'https://res.cloudinary.com/dlovq0hsk/image/upload/v1690353750/profil/CV_alternance_Donovan_Riviere_spws4x.pdf',
-    icon: 'CV',
+    url: "https://res.cloudinary.com/dlovq0hsk/image/upload/v1690359923/profil/CV_alternance_Donovan_Rivi%C3%A8re_eyfu8j.pdf",
+    icon: "CV",
   },
 ];
--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,96 +1,94 @@
-import React from 'react';
-import Footer from '../../components/Footer';
-
+import React from "react";
+import Footer from "../../components/Footer";
 
 const Projets = () => {
   return (
     <>
-      <section className='projetsSection section'>
+      <section className="projetsSection section">
         <h2>Projets</h2>
-        <div className='cardsSection'>
-          <article className='projetCard'>
+        <div className="cardsSection">
+          <article className="projetCard">
             <h4>Site Autour des jardins de Chéreng</h4>
             <p>Projet fullstack: React, NodeJS, PostgreSQL</p>
             <a
-              href='https://autourdesjardinsdechereng.netlify.app/'
-              target='_blank'
+              href="https://autourdesjardinsdechereng.netlify.app/"
+              target="_blank"
             >
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/siteADJDC_exo1vk.png'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/siteADJDC_exo1vk.png"
                 alt="page d'accueil du site Autour des jardins de chéreng"
               />
             </a>
-          </article>{' '}
-          <article className='projetCard'>
+          </article>{" "}
+          <article className="projetCard">
             <h4>changement de profil</h4>
             <p>Projet front utilisant l'API randomUser</p>
-            <a href='https://persons-profiles.netlify.app/' target='_blank'>
+            <a href="https://persons-profiles.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/profiles_jwp77y.png'
-                alt='profils de personnes'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/profiles_jwp77y.png"
+                alt="profils de personnes"
               />
             </a>
-            <a href='https://persons-profiles.netlify.app/' target='_blank'>
+            <a href="https://persons-profiles.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/profil2_son72s.png'
-                alt='profils de personnes'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/profil2_son72s.png"
+                alt="profils de personnes"
               />
             </a>
-          </article>{' '}
-          <article className='projetCard'>
+          </article>{" "}
+          <article className="projetCard">
             <h4>liste de courses</h4>
             <p>Exercice front type todo</p>
-            <a href='https://liste-courses.netlify.app/' target='_blank'>
+            <a href="https://liste-courses.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577233/profil/liste2_t6gut6.png'
-                alt='liste de courses'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577233/profil/liste2_t6gut6.png"
+                alt="liste de courses"
               />
             </a>
-            <a href='https://liste-courses.netlify.app/' target='_blank'>
+            <a href="https://liste-courses.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/listeCourses_s1glfr.png'
-                alt='liste de courses'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/listeCourses_s1glfr.png"
+                alt="liste de courses"
               />
             </a>
-          </article>{' '}
-          <article className='projetCard'>
+          </article>{" "}
+          <article className="projetCard">
             <h4>Menu</h4>
             <p>
-              Exercice front pour afficher un menu filtrable. Page responsive.{' '}
+              Exercice front pour afficher un menu filtrable. Page responsive.{" "}
             </p>
-            <a href='https://menu-diner.netlify.app/' target='_blank'>
+            <a href="https://menu-diner.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/menu_wittc6.png'
-                alt='menu'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1679577234/profil/menu_wittc6.png"
+                alt="menu"
               />
             </a>
-          </article>{' '}
-          <article className='projetCard'>
+          </article>{" "}
+          <article className="projetCard">
             <h4>Générateur aléatoire de cocktails</h4>
             <p>Projet front avec Vuejs</p>
             <a
-              href='https://cocktails-from-donovan-to-vade.netlify.app/'
-              target='_blank'
+              href="https://cocktails-from-donovan-to-vade.netlify.app/"
+              target="_blank"
             >
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1686996560/profil/cocktailsRandom_qv4f2k.png'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1686996560/profil/cocktailsRandom_qv4f2k.png"
                 alt="page d'accueil du générateur de cocktails"
               />
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1686996666/profil/singleCocktail_xryfxw.png'
-                alt='page single cocktail'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1686996666/profil/singleCocktail_xryfxw.png"
+                alt="page single cocktail"
               />
             </a>
-          </article>{' '}
-          <article className='projetCard'>
+          </article>
+          <article className="projetCard">
             <h4>Les langues des pays</h4>
-            <p>Projet avec pagination et recherche de pays par langue en React</p>
-            <a
-              href='https://countries-languages.netlify.app/'
-              target='_blank'
-            >
+            <p>
+              Projet avec pagination et recherche de pays par langue en React
+            </p>
+            <a href="https://countries-languages.netlify.app/" target="_blank">
               <img
-                src='https://res.cloudinary.com/dlovq0hsk/image/upload/v1689437931/profil/countries.png'
+                src="https://res.cloudinary.com/dlovq0hsk/image/upload/v1689437931/profil/countries.png"
                 alt="page de recherche"
               />
             </a>
