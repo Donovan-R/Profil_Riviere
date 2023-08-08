2d18466fb3486085c532560a1a80c6a655283391
Donovan <donoriviere@gmail.com>
2023-08-09 00:13:33 +0200

    change swiper config

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -42,117 +42,146 @@ const Experiences = () => {
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                Depuis le 11/04/2018 : agent d'exploitation dans un parc de
+                <span className="expDate">Depuis le 11/04/2018 :</span>
+                <br /> agent d'exploitation qualifié dans un parc de
                 stationnement de la société Indigo
               </h4>
-              <ul>
-                <li>Accueil des clients.</li>
-                <li>Petite maintenance.</li>
-                <li>Sécurité incendie et assistance aux personnes</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>Accueil des clients.</li>
+                  <li>Petite maintenance.</li>
+                  <li>Sécurité incendie et assistance aux personnes</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                De décembre 2015 à avril 2018 : assistant de recherche pour le
-                CNRS (J-Pal Europe)
+                <span className="expDate">
+                  {" "}
+                  De décembre 2015 à avril 2018 :
+                </span>
+                <br /> assistant de recherche pour le CNRS (J-Pal Europe)
               </h4>
-              <ul>
-                <li>
-                  Intermédiaire entre le chef de projet et la structure enquêtée
-                </li>
-                <li>Administration de questionnaires</li>
-                <li>Extraction et traitement de données via Stata</li>
-                <li>Observation des pratiques professionnelles</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>
+                    Intermédiaire entre le chef de projet et la structure
+                    enquêtée
+                  </li>
+                  <li>Administration de questionnaires</li>
+                  <li>Observation des pratiques professionnelles</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                De septembre 2016 à avril 2017: tuteur pédagogique au sein de
-                l'université Lille3
+                <span className="expDate">
+                  {" "}
+                  De septembre 2016 à avril 2017 :
+                </span>
+                <br /> tuteur pédagogique au sein de l'université Lille3
               </h4>
-              <ul>
-                {" "}
-                <li>
-                  Enseignement de matières relevant de la linguistique auprès
-                  d'étudiants en première année de licence
-                </li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  {" "}
+                  <li>
+                    Enseignement de matières relevant de la linguistique auprès
+                    d'étudiants en première année de licence
+                  </li>
+                  <li>Conception et animation de cours</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                De 2011 à avril 2018: intérimaire pour Kelly-Services et Crit
+                <span className="expDate">De 2011 à avril 2018 : </span>
+                <br />
+                intérimaire pour Kelly-Services et Crit
               </h4>
-              <ul>
-                <li>Manutention (Castorama, BTR services)</li>
-                <li>Agent d'exploitation (Vinci Park, Indigo, Autocité)</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>Manutention (Castorama, BTR services)</li>
+                  <li>Agent d'exploitation (Vinci Park, Indigo, Autocité)</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                De septembre 2012 à juin 2013: enseignant de français (FLE) à
-                Antananarivo au sein du centre de perfectionnement des officiers
-                (CPO) et de l'école d'état major (EEM)
+                <span className="expDate">
+                  {" "}
+                  De septembre 2012 à juin 2013 :
+                </span>
+                <br />
+                enseignant de français (FLE) à Antananarivo
               </h4>
-              <ul>
-                <li>
-                  Enseignement de la langue française à des adultes militaires
-                </li>
-                <li>
-                  Préparation aux examens du cours de français et de l'alliance
-                  française (DELF/ DALF)
-                </li>
-                <li>Élaboration de cours et d'évaluations</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>
+                    Enseignement de la langue française à des adultes militaires
+                  </li>
+                  <li>
+                    Préparation aux examens du cours de français et de
+                    l'alliance française (DELF/ DALF)
+                  </li>
+                  <li>Élaboration de cours et d'évaluations</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                Périodes estivales de 2010, 2011, 2012 et 2013: animateur
-                linguistique (taal-trainer animator) à Courtrai pour l'organisme
-                BLCC Kids, au sein de l'université de la Kulak
+                <span className="expDate">
+                  Périodes estivales de 2010 à 2013 :{" "}
+                </span>
+                <br /> Taal-trainer animator à Courtrai pour BLCC Kids
               </h4>
-              <ul>
-                <li>Enseignement de la langue française à des enfants</li>
-                <li>Élaboration d'activités ludo-éducatives</li>
-                <li>
-                  Animation et élaboration de jeux intérieurs et extérieurs
-                </li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>Enseignement de la langue française à des enfants</li>
+                  <li>Élaboration d'activités ludo-éducatives</li>
+                  <li>
+                    Animation et élaboration de jeux intérieurs et extérieurs
+                  </li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                D'octobre 2010 à juillet 2011: enseignant de français (FLE) à
-                Bucarest au sein de l'académie militaire Carol I et du ministère
-                de la défense
+                <span className="expDate">D'octobre 2010 à juillet 2011 :</span>
+                <br /> enseignant de français (FLE) à Bucarest
               </h4>
-              <ul>
-                <li>Enseignement de la langue française</li>
-                <li>conception pédagogique</li>
-                <li>organisation des épreuves</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>Enseignement de la langue française</li>
+                  <li>conception pédagogique</li>
+                  <li>organisation des épreuves</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="expCard">
               <h4 className="expTitle">
-                Du 16/12/2005 au 15/08/2010: équipier polyvalent en restauration
-                rapide (Quick et KFC)
+                <span className="expDate">Du 16/12/2005 au 15/08/2010 :</span>
+                <br /> équipier polyvalent en restauration rapide (Quick et KFC)
               </h4>
-              <ul>
-                <li>Préparation et anticipation des commandes</li>
-                <li>Délégation de tâches aux équipiers</li>
-              </ul>
+              <div className="expDetails">
+                <ul>
+                  <li>Préparation et anticipation des commandes</li>
+                  <li>Délégation de tâches aux équipiers</li>
+                </ul>
+              </div>
             </div>
           </SwiperSlide>
         </Swiper>
